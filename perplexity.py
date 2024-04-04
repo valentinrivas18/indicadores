7c500a197727c49d1d3f22bdccf9cbcd85fd3d10
valentinrivas18 <valentin.rivas12@gmail.com>
2024-04-04 10:28:13 -0400

    se agrego la tabla en la consola

--- perplexity.py
+++ perplexity.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import pandas as pd
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import ttk
 
 # Conexión a la base de datos MySQL
 conn = mysql.connector.connect(
@@ -30,8 +32,14 @@ print(type(total_solicitudes))
 
 # Calcular el porcentaje de solicitudes por número de solicitud
 porcentaje_solicitudes = (solicitudes_por_numero / total_solicitudes) * 100
+porcentaje_solicitudes = porcentaje_solicitudes.round(2)
+resultados = pd.DataFrame({
+    'Numero de Solicitud': solicitudes_por_numero.index,
+    'Cantidad': solicitudes_por_numero.values,
+    'Porcentaje': porcentaje_solicitudes.values
+})
 
-print(porcentaje_solicitudes)
+print(resultados)
 
 # Generar un gráfico de barras con los porcentajes
 porcentaje_df = porcentaje_solicitudes.reset_index()
@@ -44,6 +52,8 @@ plt.ylabel('Porcentaje')
 plt.title('Porcentaje de Solicitudes por Número de Solicitud en Carrera Ingenieria en Petroleo')
 plt.xticks(range(min(porcentaje_df['id_solicitud']), max(porcentaje_df['id_solicitud'])+1))
 plt.show()
+
+
 
 print("Se mostro el matplot")
 
