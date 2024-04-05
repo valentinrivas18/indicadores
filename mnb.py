26875bb84811179a813a62ea9bd814b20e71df3e
valentinrivas18 <valentin.rivas12@gmail.com>
2024-04-04 23:53:21 -0400

    codigo changed

--- mnb.py
+++ mnb.py
@@ -2,9 +2,40 @@ from reportlab.platypus import Table, TableStyle
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.lib import colors
+import mysql.connector
 
-encabezados = [['Columna 1', 'Columna 2', "Columna 3"]]
-filas = [[1,2,3], [4,5,6], [7,8,9]]
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="valentin",
+    database="indic",
+    port="3307"
+    )
+
+cursor = conexion.cursor()
+query1 = "SELECT * FROM solicitudes"
+cursor.execute(query1)
+resultado = cursor.fetchall()
+resultado = list(resultado)
+resultado1 = [list(tup) for tup in resultado]
+
+query2 = "SELECT COUNT(*) as total from solicitudes"
+cursor.execute(query2)
+total = cursor.fetchall()
+total_ent = [int(x) for tup in total for x in tup]
+t = int(total_ent[0])
+print(t)
+
+rango = [num for num in range (0,t)]
+filas =[]
+for i in resultado1:
+    for j in rango:
+            filas.append(resultado1[j])
+filasn = list(set(map(tuple, filas)))
+print(filasn)
+
+encabezados = [['ID', 'Solicitud', "Cantidad", "Porcentaje"]]
+filas = [resultado1[0],resultado1[1]]
 
 tabla = Table(encabezados + filas)
 # Agregar bordes a la tabla
